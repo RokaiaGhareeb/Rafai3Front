816438deff61440bdaa8deffd31236ce7a504dd8
ahmedbassal <ahmed.bassal.italy@gmail.com>
2021-03-18 17:43:45 +0200

    user orders

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from 'src/services/user/user.service';
 
 @Component({
   selector: 'app-login-form',
@@ -7,9 +9,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userServ:UserService) { }
 
   ngOnInit(): void {
   }
+
+
+
+  ngOnDestroy(): void {
+
+  }
+
+
+  loginForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(20)]),
+    password: new FormControl('', [Validators.required, Validators.maxLength(20), Validators.minLength(5)])
+  })
+
+  subscriber;
+
+  faliedLogin: string = "";
+
+  login(_user, _pass) {
+
+    const loginUserInfo = { username: _user.value, password: _pass.value }
+
+    console.log(loginUserInfo)
+
+    this.subscriber = this.userServ.login(loginUserInfo).subscribe(
+
+      (data) => {
+        console.log(data)
+        localStorage.setItem('rafai3Token', data["token"]);
+
+        // //make page refresh again when navigate from logged in to check authorization and hide
+        // // logged in and register buttons
+        // this.router.navigate(['home'])
+        //   .then(() => {
+        //     window.location.reload();
+        //   });
+
+      },
+      (err) => {
+
+        console.log(err['error'])
+        this.faliedLogin = err['error'].error;
+      },
+      () => {
+ 
+        this.subscriber.unsubscribe();
+      }
+
+    )
+
+  }
+
+
+
 
 }
