5a22c4bd0b2f154245884f4e9fcd9b9be95a17d8
ahmedbassal <ahmed.bassal.italy@gmail.com>
2021-03-19 19:34:47 +0200

    some routes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { AdminService } from 'src/services/admin/admin.service';
+import { UserService } from 'src/services/user/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,21 +10,66 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private router:ActivatedRoute) { }
+  constructor(private router:ActivatedRoute,private adminServ:AdminService,private userServ:UserService) { 
 
+  }
+
+
+  subscriber;
+
+  // inject both user and admin service then check if user logged in or not and same for admin
+  //   to display navbar related to each one ((and add log in or log out in same logic))
   ngOnInit(): void {
 
-  // check if 
+  // check if route satart with admin
  this.checkIfAdmin= window.location.href.includes('admin')
 
- console.log(this.checkIfAdmin)
+ // get data of admin to display .. Admin and hist name
+ if(this.checkIfAdmin){
+
+  
+
+ }
 
 
+
+// get data from user to display welcome and name of user
+ if(!this.checkIfAdmin){
+
+ this.subscriber= this.userServ.getUserDetails().subscribe(
+
+    (data)=>{
+
+      console.log(data)
+      this.userDetails=data;
+    },
+    (err)=>{
+
+      console.log(err)
+    },
+    ()=>{
+      
+      this.subscriber.unsubscribe();
+    }
+  )
+
+ }
+
+  }
+
+
+  logout(){
+
+    localStorage.removeItem('rafai3Token');
+    this.userDetails=null;
+    this.adminDetails=null;
   }
 
 
   checkIfAdmin=false;
 
+  userDetails;
+  adminDetails;
 
 
 }
