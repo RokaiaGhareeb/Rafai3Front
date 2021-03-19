d38a456598c9d038a8b6fec4d74baa092b39c2b9
ahmedbassal <ahmed.bassal.italy@gmail.com>
2021-03-19 16:07:04 +0200

    some routes

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -46,7 +46,7 @@ export class CartComponent implements OnInit {
             },
             (err)=>{
 
-              console.log(err,);
+              console.log(err);
             },
             ()=>{
               this.unsubscriberForItem.unsubscribe();
