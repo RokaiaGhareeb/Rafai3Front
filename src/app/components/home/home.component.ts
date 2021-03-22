b61f210ed3a245c62e91a9783bd01f240d686f36
Rokaia Ghareeb <Rokaiaessam97@gmail.com>
2021-03-22 16:06:19 +0200

    added user pormotions

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/services/product.service';
+import { PromotionService } from 'src/services/promotion/promotion.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private promotionService: PromotionService, private productService: ProductService) { }
+  promotions:any;
+  products=[];
   ngOnInit(): void {
+    let unsubscribe = this.promotionService.getAll().subscribe(
+      (res)=>{
+        this.promotions = res;
+
+      },
+      (err)=>{
+        console.log(err);
+      },
+      () => {
+        this.getProducts();
+        unsubscribe.unsubscribe();
+      }
+    );
+
   }
 
+  getProducts(){
+    for(var i=0; i < this.promotions.length; i++) {
+      let unsubscribe2 = this.productService.get(this.promotions[i].productId).subscribe(
+        (res)=>{
+          this.products.push(res['product']);
+        },
+        (err)=>{
+          console.log(err);
+        },
+        ()=>{
+          unsubscribe2.unsubscribe();
+        }
+      );
+    }
+    console.log(this.products);
+  }
 }
